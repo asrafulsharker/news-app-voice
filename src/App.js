e6fdc6de9091d60501c5f98fd15059e6fb98194f
Md Asraful Sharker  Nirob <64266026+asrafulnirob1376@users.noreply.github.com>
2021-02-10 19:09:18 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
   useEffect(() => {
     alanBtn({
     //   key: '64370f4c903e66c5b517887fefa45c1b2e956eca572e1d8b807a3e2338fdd0dc/stage',
-         key :'e67a5ba179fb748137b9947285bc68922e956eca572e1d8b807a3e2338fdd0dc/stage',
+  
       onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
           setNewsArticles(articles);
@@ -95,6 +95,74 @@ const App = () => {
 };
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
